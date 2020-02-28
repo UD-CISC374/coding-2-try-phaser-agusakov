585e03616bcde926f68c12d88f7fb61b4059ed9b
Alena Gusakov <agusakov@udel.edu>
2020-02-28 12:00:26 -0500

    bouncing off the walls

--- src/scripts/objects/ballShip.ts
+++ src/scripts/objects/ballShip.ts
@@ -2,8 +2,8 @@ export default class BallShip extends Phaser.GameObjects.Sprite {
     xSpeed: number;
     ySpeed: number;
 
-    constructor(scene: Phaser.Scene, x: number, y: number, xSpeed: number, ySpeed: number) {
-        super(scene, x, y, 'ship');
+    constructor(scene: Phaser.Scene, image: string, x: number, y: number, xSpeed: number, ySpeed: number) {
+        super(scene, x, y, image);
         scene.add.existing(this);
         this.xSpeed = xSpeed;
         this.ySpeed = ySpeed;
--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -15,20 +15,20 @@ export default class MainScene extends Phaser.Scene {
   create() {
     this.background = this.add.image(0,0,"background");
     this.background.setOrigin(0,0);
-    this.ship1 = new BallShip(this, this.scale.width / 2 - 50, this.scale.height / 2, 1, 1);
+    this.ship1 = new BallShip(this, "ship", this.scale.width / 2 - 50, this.scale.height / 2, -1, 1);
+    this.ship2 = new BallShip(this, "ship2", this.scale.width / 2, this.scale.height / 2, -1, 2);
     //this.ship1 = this.add.image(this.scale.width / 2 - 50, this.scale.height / 2, "ship");
-    this.ship2 = this.add.image(this.scale.width / 2, this.scale.height / 2, "ship2");
-    this.ship3 = this.add.image(this.scale.width / 2 + 50, this.scale.height / 2, "ship");
-
+    //this.ship2 = this.add.image(this.scale.width / 2, this.scale.height / 2, "ship2");
+    this.ship3 = new BallShip(this, "ship3", this.scale.width / 2 + 50, this.scale.height / -1, 2, 2);
   }
 
   moveShip(ship) {
     ship.x += ship.xSpeed;
     ship.y += ship.ySpeed;
-    if ((ship.x == this.scale.width)||(ship.x == 0)) {
+    if ((ship.x >= this.scale.width)||(ship.x <= 0)) {
       ship.xSpeed = -ship.xSpeed;
     }
-    if ((ship.y == this.scale.height)||(ship.y == 0)) {
+    if ((ship.y >= this.scale.height)||(ship.y <= 0)) {
       ship.ySpeed = -ship.ySpeed;
     }
   }
@@ -40,5 +40,7 @@ export default class MainScene extends Phaser.Scene {
   update() {
     this.add.text(20, 20, "Playing game", {font: "25px Arial", fill: "yellow"})
     this.moveShip(this.ship1);
+    this.moveShip(this.ship2);
+    this.moveShip(this.ship3);
   }
 }
