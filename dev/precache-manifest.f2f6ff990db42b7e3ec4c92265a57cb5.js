cdc0f8e34d47a2656bfb947f482e36811a9d7076
Alena Gusakov <agusakov@udel.edu>
2020-03-02 11:04:36 -0500

    Updates

---
+++ dev/precache-manifest.f2f6ff990db42b7e3ec4c92265a57cb5.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f9e62a698f519f563594d2168ef4b73",
+    "url": "/coding-2-try-phaser-agusakov/devassets/.DS_Store"
+  },
+  {
+    "revision": "deec73c79c02e771f5fe18be9c63cf89",
+    "url": "/coding-2-try-phaser-agusakov/devassets/citations.md"
+  },
+  {
+    "revision": "194577a7e20bdcc7afbb718f502c134c",
+    "url": "/coding-2-try-phaser-agusakov/devassets/images/.DS_Store"
+  },
+  {
+    "revision": "9fd0dcea47820063efe7dabd12e32b87",
+    "url": "/coding-2-try-phaser-agusakov/devassets/images/background.png"
+  },
+  {
+    "revision": "ccd0f68c9dc7cd68ea1e98bf43aa7c90",
+    "url": "/coding-2-try-phaser-agusakov/devassets/images/ship.png"
+  },
+  {
+    "revision": "ea0ad2cda5a9dbbe332fd61e4c2f5700",
+    "url": "/coding-2-try-phaser-agusakov/devassets/images/ship2.png"
+  },
+  {
+    "revision": "5896d72a8a287a6e5f3272bfb9eadca8",
+    "url": "/coding-2-try-phaser-agusakov/devassets/images/ship3.png"
+  },
+  {
+    "revision": "a06031f1b6226db78cd533c6ebfc1328",
+    "url": "/coding-2-try-phaser-agusakov/devassets/spritesheets/Download49807.png"
+  },
+  {
+    "revision": "ea801e18a4f14b0c4740b555536b414e",
+    "url": "/coding-2-try-phaser-agusakov/devassets/spritesheets/explosion.png"
+  },
+  {
+    "revision": "9331990bf5d427ff7dba67ca4b908cd1",
+    "url": "/coding-2-try-phaser-agusakov/devassets/spritesheets/larry_slash_left.png"
+  },
+  {
+    "revision": "fe81e08e8a1a16d117a1fac9b96094ec",
+    "url": "/coding-2-try-phaser-agusakov/devassets/spritesheets/larry_walkin_left.png"
+  },
+  {
+    "revision": "69dbe1e6a105f12b472e4dbbe7172b0b",
+    "url": "/coding-2-try-phaser-agusakov/devassets/spritesheets/larry_walkin_right.png"
+  },
+  {
+    "revision": "6387e4bedb194c28efe573c1cc5ce303",
+    "url": "/coding-2-try-phaser-agusakov/devassets/spritesheets/ship.png"
+  },
+  {
+    "revision": "4888982b102ce1a7c57fb9f8c129e63d",
+    "url": "/coding-2-try-phaser-agusakov/devassets/spritesheets/ship2.png"
+  },
+  {
+    "revision": "15d30c88882683af9f5092c13dc13853",
+    "url": "/coding-2-try-phaser-agusakov/devassets/spritesheets/ship3.png"
+  },
+  {
+    "revision": "997d124fed38c88d9b60",
+    "url": "/coding-2-try-phaser-agusakov/devgen.main.84a4e253af5257f4a927.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/coding-2-try-phaser-agusakov/devgen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "c2735ff75ebc99d4119010a996e723b7",
+    "url": "/coding-2-try-phaser-agusakov/devindex.html"
+  },
+  {
+    "revision": "fc8cefcfb102b35601f09d51ee2512ef",
+    "url": "/coding-2-try-phaser-agusakov/devmanifest.json"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "/coding-2-try-phaser-agusakov/devsw.js"
+  }
+]);
