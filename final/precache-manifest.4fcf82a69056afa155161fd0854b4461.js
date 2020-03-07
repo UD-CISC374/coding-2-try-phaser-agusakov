6c305eef1bb22613d338d1e7c2428c61fee3513f
Alena Gusakov <agusakov@udel.edu>
2020-03-06 22:29:28 -0500

    Updates

---
+++ final/precache-manifest.4fcf82a69056afa155161fd0854b4461.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f9e62a698f519f563594d2168ef4b73",
+    "url": "/coding-2-try-phaser-agusakov/finalassets/.DS_Store"
+  },
+  {
+    "revision": "deec73c79c02e771f5fe18be9c63cf89",
+    "url": "/coding-2-try-phaser-agusakov/finalassets/citations.md"
+  },
+  {
+    "revision": "194577a7e20bdcc7afbb718f502c134c",
+    "url": "/coding-2-try-phaser-agusakov/finalassets/images/.DS_Store"
+  },
+  {
+    "revision": "9b0b8af047393644c175a7693f2ac93c",
+    "url": "/coding-2-try-phaser-agusakov/finalassets/images/Download60303.png"
+  },
+  {
+    "revision": "e88f6d32f68e9ac4bc3d52dfaf8d0236",
+    "url": "/coding-2-try-phaser-agusakov/finalassets/images/aqueduct section.jpg"
+  },
+  {
+    "revision": "8a78a16ee4870ff7beeac6263f509984",
+    "url": "/coding-2-try-phaser-agusakov/finalassets/images/background.png"
+  },
+  {
+    "revision": "e1753271dfb37dd3f7bd2dd1bc27eced",
+    "url": "/coding-2-try-phaser-agusakov/finalassets/images/hacking_club.jpeg"
+  },
+  {
+    "revision": "a10aea699034cc8c79667a9e4a56abc6",
+    "url": "/coding-2-try-phaser-agusakov/finalassets/images/reconstructed aqueduct.jpg"
+  },
+  {
+    "revision": "ccd0f68c9dc7cd68ea1e98bf43aa7c90",
+    "url": "/coding-2-try-phaser-agusakov/finalassets/images/ship.png"
+  },
+  {
+    "revision": "ea0ad2cda5a9dbbe332fd61e4c2f5700",
+    "url": "/coding-2-try-phaser-agusakov/finalassets/images/ship2.png"
+  },
+  {
+    "revision": "5896d72a8a287a6e5f3272bfb9eadca8",
+    "url": "/coding-2-try-phaser-agusakov/finalassets/images/ship3.png"
+  },
+  {
+    "revision": "a06031f1b6226db78cd533c6ebfc1328",
+    "url": "/coding-2-try-phaser-agusakov/finalassets/spritesheets/Download49807.png"
+  },
+  {
+    "revision": "ea801e18a4f14b0c4740b555536b414e",
+    "url": "/coding-2-try-phaser-agusakov/finalassets/spritesheets/explosion.png"
+  },
+  {
+    "revision": "9331990bf5d427ff7dba67ca4b908cd1",
+    "url": "/coding-2-try-phaser-agusakov/finalassets/spritesheets/larry_slash_left.png"
+  },
+  {
+    "revision": "e774023de59686f0262052f9817ffe40",
+    "url": "/coding-2-try-phaser-agusakov/finalassets/spritesheets/larry_slash_right.png"
+  },
+  {
+    "revision": "fe81e08e8a1a16d117a1fac9b96094ec",
+    "url": "/coding-2-try-phaser-agusakov/finalassets/spritesheets/larry_walkin_left.png"
+  },
+  {
+    "revision": "69dbe1e6a105f12b472e4dbbe7172b0b",
+    "url": "/coding-2-try-phaser-agusakov/finalassets/spritesheets/larry_walkin_right.png"
+  },
+  {
+    "revision": "dcb266cb0b9d1e515d7e64540a684e87",
+    "url": "/coding-2-try-phaser-agusakov/finalassets/spritesheets/robot_walkin.png"
+  },
+  {
+    "revision": "6387e4bedb194c28efe573c1cc5ce303",
+    "url": "/coding-2-try-phaser-agusakov/finalassets/spritesheets/ship.png"
+  },
+  {
+    "revision": "4888982b102ce1a7c57fb9f8c129e63d",
+    "url": "/coding-2-try-phaser-agusakov/finalassets/spritesheets/ship2.png"
+  },
+  {
+    "revision": "15d30c88882683af9f5092c13dc13853",
+    "url": "/coding-2-try-phaser-agusakov/finalassets/spritesheets/ship3.png"
+  },
+  {
+    "revision": "ff495487015884f98c0d",
+    "url": "/coding-2-try-phaser-agusakov/finalgen.main.61463420eef6f98df3db.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/coding-2-try-phaser-agusakov/finalgen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "b2a30035a1ae02fc43e1c8706de52773",
+    "url": "/coding-2-try-phaser-agusakov/finalindex.html"
+  },
+  {
+    "revision": "fc8cefcfb102b35601f09d51ee2512ef",
+    "url": "/coding-2-try-phaser-agusakov/finalmanifest.json"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "/coding-2-try-phaser-agusakov/finalsw.js"
+  }
+]);
---
+++ precache-manifest.b35fddd5d13c2c7e36df231e1e61edfc.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f9e62a698f519f563594d2168ef4b73",
+    "url": "assets/.DS_Store"
+  },
+  {
+    "revision": "deec73c79c02e771f5fe18be9c63cf89",
+    "url": "assets/citations.md"
+  },
+  {
+    "revision": "194577a7e20bdcc7afbb718f502c134c",
+    "url": "assets/images/.DS_Store"
+  },
+  {
+    "revision": "9b0b8af047393644c175a7693f2ac93c",
+    "url": "assets/images/Download60303.png"
+  },
+  {
+    "revision": "e88f6d32f68e9ac4bc3d52dfaf8d0236",
+    "url": "assets/images/aqueduct section.jpg"
+  },
+  {
+    "revision": "8a78a16ee4870ff7beeac6263f509984",
+    "url": "assets/images/background.png"
+  },
+  {
+    "revision": "e1753271dfb37dd3f7bd2dd1bc27eced",
+    "url": "assets/images/hacking_club.jpeg"
+  },
+  {
+    "revision": "a10aea699034cc8c79667a9e4a56abc6",
+    "url": "assets/images/reconstructed aqueduct.jpg"
+  },
+  {
+    "revision": "ccd0f68c9dc7cd68ea1e98bf43aa7c90",
+    "url": "assets/images/ship.png"
+  },
+  {
+    "revision": "ea0ad2cda5a9dbbe332fd61e4c2f5700",
+    "url": "assets/images/ship2.png"
+  },
+  {
+    "revision": "5896d72a8a287a6e5f3272bfb9eadca8",
+    "url": "assets/images/ship3.png"
+  },
+  {
+    "revision": "a06031f1b6226db78cd533c6ebfc1328",
+    "url": "assets/spritesheets/Download49807.png"
+  },
+  {
+    "revision": "ea801e18a4f14b0c4740b555536b414e",
+    "url": "assets/spritesheets/explosion.png"
+  },
+  {
+    "revision": "9331990bf5d427ff7dba67ca4b908cd1",
+    "url": "assets/spritesheets/larry_slash_left.png"
+  },
+  {
+    "revision": "e774023de59686f0262052f9817ffe40",
+    "url": "assets/spritesheets/larry_slash_right.png"
+  },
+  {
+    "revision": "fe81e08e8a1a16d117a1fac9b96094ec",
+    "url": "assets/spritesheets/larry_walkin_left.png"
+  },
+  {
+    "revision": "69dbe1e6a105f12b472e4dbbe7172b0b",
+    "url": "assets/spritesheets/larry_walkin_right.png"
+  },
+  {
+    "revision": "dcb266cb0b9d1e515d7e64540a684e87",
+    "url": "assets/spritesheets/robot_walkin.png"
+  },
+  {
+    "revision": "6387e4bedb194c28efe573c1cc5ce303",
+    "url": "assets/spritesheets/ship.png"
+  },
+  {
+    "revision": "4888982b102ce1a7c57fb9f8c129e63d",
+    "url": "assets/spritesheets/ship2.png"
+  },
+  {
+    "revision": "15d30c88882683af9f5092c13dc13853",
+    "url": "assets/spritesheets/ship3.png"
+  },
+  {
+    "revision": "44faacdd815f959387c4737db9e4daab",
+    "url": "index.html"
+  },
+  {
+    "revision": "b9f55b96ef0a62729935",
+    "url": "main.aca787bea70e3ca75ca8.bundle.js"
+  },
+  {
+    "revision": "fc8cefcfb102b35601f09d51ee2512ef",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "sw.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "vendors.a047ee0740c7d21714d9.bundle.js"
+  }
+]);
